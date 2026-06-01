f79a309203c7faf378ffc45c53ddc58b158721b7
grafitico <135188947+grafitico@users.noreply.github.com>
2026-06-01 21:55:47 +0200

    Actualizar catálogo desde panel admin

--- data.js
+++ data.js
@@ -37,17 +37,6 @@ window.__TRACKS__   = [
     "cover": "",
     "colorA": "#d08e30",
     "colorB": "#e8520f"
-  },
-  {
-    "id": "t05",
-    "title": "Brisas del Pacífico",
-    "artist": "Manuel del Mar",
-    "album": "Mar Adentro",
-    "duration": "4:50",
-    "audio": "",
-    "cover": "",
-    "colorA": "#2c56c0",
-    "colorB": "#d08e30"
   }
 ];
 
